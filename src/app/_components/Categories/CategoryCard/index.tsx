2914059754f920c1af92a32cf1bee6a42aad8871
G-KIBICHII <kibichiigilbert54@gmail.com>
2025-04-13 16:07:27 +0300

    message

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -1,8 +1,39 @@
+// 'use client'
+// import React from 'react'
+// import Link from 'next/link'
+
+// import { Category } from '../../../../payload/payload-types'
+// import { useFilter } from '../../../_providers/Filter'
+
+// import classes from './index.module.scss'
+
+// type CategoryCardProps = {
+//   category: Category
+// }
+
+// const CategoryCard = ({ category }: CategoryCardProps) => {
+//   const media = category.media as Media
+//   const { setCategoryFilters } = useFilter()
+
+//   return (
+//     <Link
+//       href="/products"
+//       className={classes.card}
+//       style={{ backgroundImage: `url(${media.url})` }}
+//       onClick={() => setCategoryFilters([category.id])}
+//     >
+//       <p className={classes.title}>{category.title}</p>
+//     </Link>
+//   )
+// }
+
+// export default CategoryCard
+
 'use client'
 import React from 'react'
 import Link from 'next/link'
 
-import { Category } from '../../../../payload/payload-types'
+import { Category, Media } from '../../../../payload/payload-types'
 import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
@@ -12,14 +43,16 @@ type CategoryCardProps = {
 }
 
 const CategoryCard = ({ category }: CategoryCardProps) => {
-  const media = category.media as Media
+  const media = category.media as Media | null
   const { setCategoryFilters } = useFilter()
+
+  const backgroundImage = media?.url ? `url(${media.url})` : undefined // Or provide a fallback image if desired
 
   return (
     <Link
       href="/products"
       className={classes.card}
-      style={{ backgroundImage: `url(${media.url})` }}
+      style={backgroundImage ? { backgroundImage } : {}}
       onClick={() => setCategoryFilters([category.id])}
     >
       <p className={classes.title}>{category.title}</p>
